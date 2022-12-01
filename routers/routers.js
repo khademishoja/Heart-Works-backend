fd02cb97d794a2c0ff636b7977b47c8cd52adfab
khademishoja <khademishoja@gmail.com>
2022-12-01 09:56:21 +0100

    validates that the new bid is the highest bid

--- routers/routers.js
+++ routers/routers.js
@@ -35,10 +35,31 @@ router.put("/artworks/:id", async (req, res, next) => {
     next(e);
   }
 });
-router.post("/bids", authMiddleWare, async (req, res, next) => {
+router.post("/bids", async (req, res, next) => {
   try {
     console.log(req.body);
     const newBid = req.body;
+    const artWork = await Artwork.findByPk(newBid.artworkId, {
+      include: [Bid],
+    });
+
+    if (artWork.bids.length === 0) {
+      console.log(artWork.minimumBid);
+      if (newBid.amount === 0 || artWork.minimumBid >= newBid.amount) {
+        return res.status(404).send({
+          message: ` The minimum value of the input for amount is higher minimum bid is.`,
+        });
+      }
+    }
+    const amounts = artWork.bids.map((bid) => {
+      return bid.amount;
+    });
+    const maxAmount = Math.max(...amounts);
+    if (maxAmount >= newBid.amount)
+      return res.status(404).send({
+        message: `you have to enter more than ${maxAmount}`,
+      });
+
     const bid = await Bid.create(newBid);
     res.send(bid);
   } catch (e) {
